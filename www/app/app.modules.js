74a70f0f643de26d00fbba6e3bb67a88c2c7eeed
cmtp <christian@hotmail.com>
2016-10-05 23:20:00 -0400

    add new folder with app

---
+++ www/app/app.modules.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('app', [
+        //Core
+        'ionic',
+        //Modules
+        'app.activity',
+        'app.camera',
+        'app.home',
+        'app.map',
+        'app.search',
+        'app.tabs',
+        'app.user'
+    ]);
+})();
---
+++ www/app/app.routes.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular.module('app')
+    .config( routerConfig );
+
+    function routerConfig($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+        // CODE
+    }
+})();
